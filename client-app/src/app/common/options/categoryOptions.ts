30d21dfe869553f26d48642a888ff7f585cdc505
Marvei.Pana <nkcc12345@gmail.com>
2024-05-23 01:51:34 +0800

    End of Section 11

---
+++ client-app/src/app/common/options/categoryOptions.ts
@@ -0,0 +1,8 @@
+export const categoryOptions = [
+    {text: 'Drinks', value: 'drinks'},
+    {text: 'Culture', value: 'culture'},
+    {text: 'Film', value: 'film'},
+    {text: 'Music', value: 'music'},
+    {text: 'Travel', value: 'travel'},
+    {text: 'Food', value: 'food'},
+]
--- client-app/src/features/activities/dashboard/ActivityListItem.tsx
+++ client-app/src/features/activities/dashboard/ActivityListItem.tsx
@@ -1,6 +1,7 @@
 import { Button, Icon, Item, Segment } from "semantic-ui-react";
 import { Activity } from "../../activity";
 import { Link } from "react-router-dom";
+import { format } from "date-fns";
 
 
 
@@ -26,7 +27,7 @@ export default function ActivityListItem({activity}: Props){
             </Segment>
             <Segment>
                 <span>
-                    <Icon name='clock' /> {activity.date}
+                    <Icon name='clock' /> {format(activity.date!, 'dd MMM yyyy h:mm aa')}
                     <Icon name='marker' /> {activity.venue}
                 </span>
             </Segment>
